bcd7cf88528948a17ad19fe95fd85093e3d53d0e
Martta Rantanen <rantanenmartta@gmail.com>
2026-03-24 10:37:23 +0200

    Add user pages

---
+++ users.py
@@ -0,0 +1,12 @@
+import db
+
+def get_user(user_id):
+    sql = """SELECT id, username
+             FROM users
+             WHERE id = ?"""
+    result = db.query(sql, [user_id])
+    return result[0] if result else None
+
+def get_items(user_id):
+    sql = "SELECT id, book_name FROM books WHERE user_id = ? ORDER BY id DESC"
+    return db.query(sql, [user_id])
